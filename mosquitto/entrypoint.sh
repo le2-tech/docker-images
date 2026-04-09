0dc6f965d5c0cde73ff043e9cfb1b81b1cc0cb74
蘇 <su@le2.ltd>
2026-04-09 15:23:40 +0800

    mosquitto

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 if [ "${1#-}" != "$1" ]; then
-	set -- /opt/usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf "$@"
+	set -- mosquitto -c /mosquitto/config/mosquitto.conf "$@"
 fi
 
 mkdir -p /app/data /mosquitto/data /mosquitto/log
